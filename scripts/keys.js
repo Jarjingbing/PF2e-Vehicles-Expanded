3a288aec969e7b497ceed48d275322b658d37fd8
Kivi Loverose <jarjingbing@gmail.com>
2024-11-24 01:32:02 -0600

    starcraft statistics

---
+++ scripts/keys.js
@@ -0,0 +1,42 @@
+export const frames = [
+    'racer',
+    'interceptor', '' +
+    'shuttle',
+    'fighter',
+    'light frieghter',
+    'explorer',
+    'transport',
+    'destroyer',
+    'heavy freighter',
+    'bulk freighter',
+    'cruiser',
+    'carrier',
+    'battleship',
+    'dreadnought'
+]
+
+export const starcraft = {
+    "tier": "PF2EVEHICLESEXPANDED.tier",
+    "size": "PF2EVEHICLESEXPANDED.size",
+    "frame": "PF2EVEHICLESEXPANDED.frame",
+    // Statistics
+    "spd": "PF2EVEHICLESEXPANDED.spd",
+    "mnvr": "PF2EVEHICLESEXPANDED.mnvr",
+    "drift": "PF2EVEHICLESEXPANDED.drift",
+
+    "ac": "PF2EVEHICLESEXPANDED.ac",
+    "tl": "PF2EVEHICLESEXPANDED.tl",
+
+    "hp": "PF2EVEHICLESEXPANDED.hp",
+
+    "dt": "PF2EVEHICLESEXPANDED.dt",
+    "ct": "PF2EVEHICLESEXPANDED.ct",
+
+    "shields": "PF2EVEHICLESEXPANDED.shields",
+
+    "power": "PF2EVEHICLESEXPANDED.power",
+    "engine": "PF2EVEHICLESEXPANDED.engine",
+    "sys": "PF2EVEHICLESEXPANDED.sys",
+    "bays": "PF2EVEHICLESEXPANDED.bays",
+    "cost": "PF2EVEHICLESEXPANDED.cost"
+}
---
+++ scripts/values.js
@@ -0,0 +1,146 @@
+import {starcraft} from "./keys";
+
+const frameAlias = {
+    frames: {
+        "racer": {
+            [starcraft.size]: "tiny",
+            [starcraft.frame]: "racer",
+            [starcraft.mnvr]: "perfect",
+            [starcraft.hp]: "20",
+            [starcraft.dt]: null,
+            [starcraft.ct]: "4",
+            [starcraft.bays]: null,
+            [starcraft.cost]: "4"
+        },
+        "interceptor": {
+            [starcraft.size]: "tiny",
+            [starcraft.frame]: "interceptor",
+            [starcraft.mnvr]: "perfect",
+            [starcraft.hp]: "30",
+            [starcraft.dt]: null,
+            [starcraft.ct]: "6",
+            [starcraft.bays]: null,
+            [starcraft.cost]: "6"
+        },
+        "shuttle": {
+            [starcraft.size]: "small",
+            [starcraft.frame]: "shuttle",
+            [starcraft.mnvr]: "perfect",
+            [starcraft.hp]: "35",
+            [starcraft.dt]: null,
+            [starcraft.ct]: "7",
+            [starcraft.bays]: "3",
+            [starcraft.cost]: "6"
+        },
+        "fighter": {
+            [starcraft.size]: "tiny",
+            [starcraft.frame]: "fighter",
+            [starcraft.mnvr]: "good",
+            [starcraft.hp]: "35",
+            [starcraft.dt]: null,
+            [starcraft.ct]: "7",
+            [starcraft.bays]: null,
+            [starcraft.cost]: "8"
+        },
+        "light freighter": {
+            [starcraft.size]: "small",
+            [starcraft.frame]: "light freighter",
+            [starcraft.mnvr]: "good",
+            [starcraft.hp]: "40",
+            [starcraft.dt]: null,
+            [starcraft.ct]: "8",
+            [starcraft.bays]: "3",
+            [starcraft.cost]: "10"
+        },
+        "explorer": {
+            [starcraft.size]: "medium",
+            [starcraft.frame]: "explorer",
+            [starcraft.mnvr]: "good",
+            [starcraft.hp]: "55",
+            [starcraft.dt]: null,
+            [starcraft.ct]: "11",
+            [starcraft.bays]: "4",
+            [starcraft.cost]: "12"
+        },
+        "transport": {
+            [starcraft.size]: "medium",
+            [starcraft.frame]: "transport",
+            [starcraft.mnvr]: "average",
+            [starcraft.hp]: "70",
+            [starcraft.dt]: null,
+            [starcraft.ct]: "14",
+            [starcraft.bays]: "5",
+            [starcraft.cost]: "15"
+        },
+        "destroyer": {
+            [starcraft.size]: "large",
+            [starcraft.frame]: "destroyer",
+            [starcraft.mnvr]: "average",
+            [starcraft.hp]: "150",
+            [starcraft.dt]: null,
+            [starcraft.ct]: "30",
+            [starcraft.bays]: "4",
+            [starcraft.cost]: "30"
+        },
+        "heavy freighter": {
+            [starcraft.size]: "large",
+            [starcraft.frame]: "heavy freighter",
+            [starcraft.mnvr]: "average",
+            [starcraft.hp]: "120",
+            [starcraft.dt]: null,
+            [starcraft.ct]: "24",
+            [starcraft.bays]: "8",
+            [starcraft.cost]: "40"
+        },
+        "bulk freighter": {
+            [starcraft.size]: "huge",
+            [starcraft.frame]: "heavy freighter",
+            [starcraft.mnvr]: "poor",
+            [starcraft.hp]: "160",
+            [starcraft.dt]: "5",
+            [starcraft.ct]: "32",
+            [starcraft.bays]: "10",
+            [starcraft.cost]: "55"
+        },
+        "cruiser": {
+            [starcraft.size]: "huge",
+            [starcraft.frame]: "cruiser",
+            [starcraft.mnvr]: "average",
+            [starcraft.hp]: "180",
+            [starcraft.dt]: "5",
+            [starcraft.ct]: "36",
+            [starcraft.bays]: "6",
+            [starcraft.cost]: "60"
+        },
+        "carrier": {
+            [starcraft.size]: "gargantuan",
+            [starcraft.frame]: "carrier",
+            [starcraft.mnvr]: "poor",
+            [starcraft.hp]: "240",
+            [starcraft.dt]: "10",
+            [starcraft.ct]: "48",
+            [starcraft.bays]: "10",
+            [starcraft.cost]: "120"
+        },
+        "battleship": {
+            [starcraft.size]: "gargantuan",
+            [starcraft.frame]: "battleship",
+            [starcraft.mnvr]: "average",
+            [starcraft.hp]: "280",
+            [starcraft.dt]: "10",
+            [starcraft.ct]: "56",
+            [starcraft.bays]: "8",
+            [starcraft.cost]: "150"
+        },
+        "dreadnought": {
+            [starcraft.size]: "colossal",
+            [starcraft.frame]: "dreadnought",
+            [starcraft.mnvr]: "clumsy",
+            [starcraft.hp]: "400",
+            [starcraft.dt]: "15",
+            [starcraft.ct]: "80",
+            [starcraft.bays]: "20",
+            [starcraft.cost]: "200"
+        }
+    }
+}
